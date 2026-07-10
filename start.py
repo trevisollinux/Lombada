8d5807b9e3cd787fb63e7bc52a35e3163b479fbf
trevisollinux <60004247+trevisollinux@users.noreply.github.com>
2026-07-10 17:05:59 -0300

    fix: start application through patched search entrypoint

--- start.py
+++ start.py
@@ -15,4 +15,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app_entry:app", host="0.0.0.0", port=port)
