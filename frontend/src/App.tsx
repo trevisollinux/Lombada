2c5e25146617cefac73839d8ab5178cbedfb9dda
trevisollinux <60004247+trevisollinux@users.noreply.github.com>
2026-07-16 23:25:08 -0300

    feat: instala flags e analytics no shell v2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 import { AppLayout } from './components/AppLayout'
+import { ProductAnalyticsBridge } from './components/ProductAnalyticsBridge'
 import { DiaryPage } from './pages/DiaryPage'
 import { ExplorePage } from './pages/ExplorePage'
 import { FeedPage } from './pages/FeedPage'
@@ -10,30 +11,34 @@ import { ProfilePage } from './pages/ProfilePage'
 import { SearchPage } from './pages/SearchPage'
 import { ShelfPage } from './pages/ShelfPage'
 import { WorkPage } from './pages/WorkPage'
+import { FeatureFlagsProvider } from './providers/FeatureFlagsProvider'
 import { PreferencesProvider } from './providers/PreferencesProvider'
 import { SessionProvider } from './providers/SessionProvider'
 
 export function App() {
   return (
     <PreferencesProvider>
-      <SessionProvider>
-        <BrowserRouter basename="/app-v2">
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route index element={<SearchPage />} />
-              <Route path="explorar" element={<ExplorePage />} />
-              <Route path="obra" element={<WorkPage />} />
-              <Route path="feed" element={<FeedPage />} />
-              <Route path="estante" element={<ShelfPage />} />
-              <Route path="diario" element={<DiaryPage />} />
-              <Route path="memorias" element={<MemoriesPage />} />
-              <Route path="perfil" element={<ProfilePage />} />
-              <Route path="perfil/:handle" element={<ProfilePage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </SessionProvider>
+      <FeatureFlagsProvider>
+        <SessionProvider>
+          <ProductAnalyticsBridge />
+          <BrowserRouter basename="/app-v2">
+            <Routes>
+              <Route element={<AppLayout />}>
+                <Route index element={<SearchPage />} />
+                <Route path="explorar" element={<ExplorePage />} />
+                <Route path="obra" element={<WorkPage />} />
+                <Route path="feed" element={<FeedPage />} />
+                <Route path="estante" element={<ShelfPage />} />
+                <Route path="diario" element={<DiaryPage />} />
+                <Route path="memorias" element={<MemoriesPage />} />
+                <Route path="perfil" element={<ProfilePage />} />
+                <Route path="perfil/:handle" element={<ProfilePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </SessionProvider>
+      </FeatureFlagsProvider>
     </PreferencesProvider>
   )
 }
